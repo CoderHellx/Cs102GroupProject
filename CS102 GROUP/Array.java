b853455f368aa236c456442eed5d27596d90302e
Sezin Tuncer <sezin.tuncer-ug@labs.bilkent.edu.tr>
2025-02-06 09:52:07 +0300

    array is created

--- CS102 GROUP/Array.java
+++ CS102 GROUP/Array.java
@@ -1,4 +1,18 @@
+import java.util.Random;
+
 public static void main(String[] args)
 {
     
+}
+
+public int[] createArray(int size) {
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for ( int ind = 0; ind < size; ind++ ) {
+        int randNum = rand.nextInt(101);
+        array[ind] = randNum;
+    }
+
+    return array;
 }
